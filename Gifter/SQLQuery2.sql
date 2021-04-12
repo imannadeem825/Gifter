01e9951935ae7ca263e3b0c276be72987b24ddde
Iman Nadeem <iman.nadeem.825@gmail.com>
2021-04-12 14:15:52 -0500

    first commit oops

---
+++ Gifter/SQLQuery2.sql
@@ -0,0 +1,12 @@
+﻿     SELECT p.Title, p.Caption, p.DateCreated, p.ImageUrl, p.UserProfileId,
+
+                            
+                            up.[Name], up.Bio, up.Email, up.DateCreated AS UserProfileDateCreated,
+                            up.ImageUrl AS UserProfileImageUrl,
+                            
+                           c.Id AS CommentId, c.Message, c.UserProfileId AS CommentUserProfileId
+                           FROM Post p
+                   
+                           LEFT JOIN Comment c on c.PostId = p.id
+                            LEFT JOIN UserProfile up ON p.UserProfileId = up.id
+                           WHERE p.Id = 1
